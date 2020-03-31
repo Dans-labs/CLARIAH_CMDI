9ec4f20066d20a0e9e1479a06db36a866203d737
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2020-03-31 13:02:50 +0200

    XML2Dict module created to process CMDI files

--- cmdi2dict.py
+++ cmdi2dict.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 import sys
 reload(sys)  
+import os
 sys.setdefaultencoding('utf-8')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), './')))
 from xml.dom import minidom
-from xml2dict import xmldom2dict
+from xml2dict.processor import load, xmldom2dict
 
 if __name__=='__main__':
-    data = minidom.parse(sys.argv[1])
-
-    d=xmldom2dict(data)
-
-    print d
+    #data = minidom.parse(sys.argv[1])
+    #d=xmldom2dict(data)
+    d = load(sys.argv[1])
+    print(d)
