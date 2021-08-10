c33c73d57f1610986c7496b836b56d37e8b4eabd
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2021-08-10 10:39:47 +0200

    Draftlinkage module to collect controlled vocabularies values from various sources like Wikidata and Skosmos

---
+++ xml2dict/linkage.py
@@ -0,0 +1,72 @@
+import json
+import requests
+from skosmos_client import SkosmosClient
+
+class Draftlinkage():
+    def __init__(self, sourceobject=None, content=None, debug=False):
+        self.keywords = {}
+        self.geoconcepts = {}
+        self.source = sourceobject
+        self.linkagesource = 'https://query.wikidata.org/sparql#entities-all'
+        self.apiskosmos = 'http://api.finto.fi/rest/v1/'
+        self.debug = 'True' #debug
+        self.geosource = 'yso-paikat'
+
+    def conceptmaker(self, cotype, q, s):
+        if cotype == 'nde':
+            return self.ndegrapql(q, s)
+        if cotype == 'skosmos':
+            return self.skosmosql(q, s)
+        return
+
+    def skosmosql(self, q, s):
+        results = {}
+        q = 'Amsterdam'
+        #q = 'ne*'
+        query = q + '*'
+        skosmos = SkosmosClient(api_base=self.apiskosmos)
+        results['rawdata'] = skosmos.search(query, vocabs=s, lang="en")
+        print(len(results['rawdata']))
+        concepturi = []
+        for concept in results['rawdata']:
+            if 'uri' in concept:
+                data = skosmos.get_concept(s, concept['uri'])
+                concepturi.append({ concept['uri']: str(data) })
+
+        results['uri'] = concepturi
+        return results
+
+    def ndegrapql(self, q, s):
+        results = {}
+        headers = {"content-type":"application/json"}
+        query = "{\"query\":\"query Terms {  terms(sources: [\\\"" + s + "\\\"], query: \\\"" + q + "\\\") {    source {      uri      name      creators {        uri        name        alternateName      }    }    result {      __typename      ... on Terms {        terms {          uri          prefLabel          altLabel          hiddenLabel          scopeNote          broader {            uri            prefLabel          }          narrower {            uri            prefLabel          }          related {            uri            prefLabel          }        }      }      ... on Error {        message      }    }  }}\"}"
+        r = requests.post("https://termennetwerk-api.netwerkdigitaalerfgoed.nl/graphql", data=query, headers=headers)
+        results['rawdata'] = r.json()
+        return results
+
+    def linkage(self, x):
+        if isinstance(x, list):
+            return [self.linkage(v) for v in x]
+        elif isinstance(x, dict):
+            for k, v in x.items():
+                if k == '1Keyword':
+                    for keyword in v:
+                        search = self.conceptmaker('nde', keyword, self.linkagesource)
+                        if search:
+                            if self.debug:
+                                print("%s => %s\n" % (keyword, search))
+                            self.keywords[keyword] = search
+                if k == 'SpatialCoverage':
+                    concepts = {}
+                    skossearch = self.conceptmaker('skosmos', v, self.geosource)
+                    if skossearch:
+                        concepts['skosmos'] = skossearch
+                    search = self.conceptmaker('nde', v, self.linkagesource)
+                    if search:
+                        concepts['nde'] = search
+                        if self.debug:
+                            print("%s => %s\n" % (v, search))
+                    self.geoconcepts[v] = concepts
+            return {k[0].upper() + k[1:]: self.linkage(v) for k, v in x.items()}
+        else:
+            return x
